78784c5e497c058238c580e11295dd0568af281c
inceserhatabdullah <inceserhatabdullah@gmail.com>
2026-01-10 21:32:25 +0300

    changed the endpoint name

--- app/api/v1/ingest.py
+++ app/api/v1/ingest.py
@@ -4,7 +4,7 @@ from app.services.ingest import IngestService
 router = APIRouter()
 
 
-@router.post("/file")
+@router.post("/document")
 async def upload(file: UploadFile = File(...)):
     ingest_service = IngestService()
     return await ingest_service.ingest(file)
